fd772b276b2704be8ed111996c6b66e1a409e0c4
Bryan Glass <bryanjglass@gmail.com>
2012-03-27 20:55:50 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TheDailyCloud::Application.config.secret_token = '0cc7d5ac20d6bf92a1d22d06b50ed7863bf5d101f77a66c1154362dc9980d66705f6f86a72e5173bf930c213c11df99e0c466e0268df9b53327440036814fd39'
